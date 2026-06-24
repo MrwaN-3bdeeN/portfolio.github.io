c699b49835ce0ef8ecfdbdf62b592782a019d3d7
Marwan Abdeen <marwan.abdeen1998@gmail.com>
2026-06-24 07:09:59 +0300

    Update base path to /portfolio/

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/portfolio.github.io/',
+  base: '/portfolio/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
